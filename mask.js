f27d08c4fd93e1b32667b55fdde3817a1f7b54c5
luizgmachado <luiz.gustavo.machado@hotmail.com>
2023-04-27 14:26:45 -0300

    add mask

---
+++ mask.js
@@ -0,0 +1,18 @@
+export const masks = {
+  phone(value) {
+    return value
+    .replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{4})(\d)/, '$1-$2')
+    .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+    .replace(/(-\d{4})\d+?$/, '$1')
+  }
+}
+
+document.querySelectorAll('input').forEach(($input) => {
+  const field = $input.dataset.js
+
+  $input.addEventListener('input', (e) => {
+    e.target.value = masks[field](e.target.value)
+  }, false)
+})
